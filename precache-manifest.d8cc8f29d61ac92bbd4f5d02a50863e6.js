a84f2a0c211124e24e9a1c174fa37fe3981dd175
Anfernee Rapio <rapioanfernee@gmail.com>
2019-06-11 23:55:37 +0800

    Updates

---
+++ precache-manifest.d8cc8f29d61ac92bbd4f5d02a50863e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a4b94df14ac506d41400102612b0cb",
+    "url": "/opendotaclone/index.html"
+  },
+  {
+    "revision": "099694d13791eb2379b9",
+    "url": "/opendotaclone/static/css/2.f41ad63a.chunk.css"
+  },
+  {
+    "revision": "552195ed188b1541fc93",
+    "url": "/opendotaclone/static/css/main.35730e85.chunk.css"
+  },
+  {
+    "revision": "099694d13791eb2379b9",
+    "url": "/opendotaclone/static/js/2.12822af0.chunk.js"
+  },
+  {
+    "revision": "552195ed188b1541fc93",
+    "url": "/opendotaclone/static/js/main.de7e9376.chunk.js"
+  },
+  {
+    "revision": "16311141b9667e7f6e4a",
+    "url": "/opendotaclone/static/js/runtime~main.9102f9bc.js"
+  }
+]);
